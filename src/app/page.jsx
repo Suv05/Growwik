d9d7b5e3b0b6687b0ade2f14d2a3245057cefccf
suvam <suvampravu@gmail.com>
2024-11-05 09:21:50 +0530

    Home page ui finished

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,27 @@
-
+import HeroSection from "@/components/Home/HeroSection";
+import LatestWorks from "@/components/Home/LatestWorks";
+import AboutUs from "@/components/Home/AboutUs";
+import ServicePage from "@/components/Home/ServicePage";
+import Platform from "@/components/Home/Platfrom";
+import Creators from "@/components/Home/Creators";
+import WhyChooseUse from "@/components/Home/WhyChooseUs";
+import OurClients from "@/components/Home/OurClients";
+import ContactForm from "@/components/Home/ContactForm";
+import Review from "@/components/Home/Review";
 
 export default function Home() {
   return (
-    <></>
+    <>
+    <HeroSection />
+    <LatestWorks />
+    <AboutUs />
+    <ServicePage />
+    <Platform />
+    <Creators />
+    <WhyChooseUse />
+    <Review />
+    <OurClients />
+    <ContactForm />
+    </>
   );
 }
