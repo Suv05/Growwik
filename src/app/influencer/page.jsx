85ab7551c97baa2a49c551972ed708020abd4dda
suvam <suvampravu@gmail.com>
2024-11-26 22:02:08 +0530

    influencer page design finished

---
+++ src/app/influencer/page.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "@/components/brandUI/HeroSection";
+import BrandFrom from "@/components/brandUI/BrandForm";
+
+function Influencer({}) {
+  return (
+    <>
+      <HeroSection />
+      <BrandFrom />
+    </>
+  );
+}
+
+export default Influencer;
