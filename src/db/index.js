c1f77ac4cc12caa1de562bc4b6975cf94416badf
suvam <suvampravu@gmail.com>
2024-12-05 10:43:30 +0530

    backend setup

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGO_URI;
 
 const clientOptions = {
   serverApi: { version: "1", strict: true, deprecationErrors: true },
