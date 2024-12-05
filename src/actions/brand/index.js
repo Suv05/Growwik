d76c9824cf744aa16ebbb2aadb0f545f2f326aac
suvam <suvampravu@gmail.com>
2024-12-05 10:42:57 +0530

    backend setup

---
+++ src/actions/brand/index.js
@@ -0,0 +1,30 @@
+import createConnection from "@/db";
+import Brand from "@/schema/brand.model.js";
+
+export async function setBrand(data) {
+  try {
+    // Ensure the MongoDB connection is established
+    await createConnection();
+
+    const newUser = await Brand.create(data);
+
+    if (newUser) {
+      return {
+        status: "success",
+        message: "Brand created successfully",
+      };
+    } else {
+      return {
+        status: "error",
+        message: "Brand creation failed",
+      };
+    }
+  } catch (err) {
+    console.log(err);
+
+    return {
+      status: "error",
+      message: "Brand creation failed",
+    };
+  }
+}
