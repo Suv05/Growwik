d658d54efb23453e29450c35cfe632c0c73ff7e9
suvam <suvampravu@gmail.com>
2024-12-19 21:31:49 +0530

    blog page UI complete

--- src/actions/blog/index.js
+++ src/actions/blog/index.js
@@ -3,6 +3,7 @@
 import createConnection from "@/db";
 import Blog from "@/schema/blog.model.js";
 
+//post blog
 export const postBlog = async (data) => {
   try {
     await createConnection();
@@ -25,6 +26,34 @@ export const postBlog = async (data) => {
     return {
       status: "error",
       message: "Blog creation failed",
+    };
+  }
+};
+
+//fetch all blog
+export const getAllBlog = async () => {
+  try {
+    await createConnection();
+
+    const allBlog = await Blog.find();
+
+    if (allBlog) {
+      return {
+        status: "success",
+        message: "Blog fetched successfully",
+        data: JSON.parse(JSON.stringify(allBlog)),
+      };
+    } else {
+      return {
+        status: "error",
+        message: "Blog fetching failed",
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      status: "error",
+      message: "Blog fetching failed",
     };
   }
 };
