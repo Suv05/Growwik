0ca696a4baacf6e7515c3c4a4555a824b7100c9b
suvam <suvampravu@gmail.com>
2024-12-19 21:32:15 +0530

    blog page UI complete

--- src/schema/blog.model.js
+++ src/schema/blog.model.js
@@ -1,27 +1,30 @@
 import mongoose from "mongoose";
 const { Schema, model, models } = mongoose;
 
-const blogSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-    minLenght: [3, "Title is too short"],
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minLenght: [3, "Title is too short"],
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  link: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Blog = models.Blog || model("Blog", blogSchema);
 
