4f2453d553eb804a47e52a2d6af9b763baefe7fb
suvam <suvampravu@gmail.com>
2025-02-27 00:48:44 +0530

    commit scroll effect

--- src/app/feedback/page.jsx
+++ src/app/feedback/page.jsx
@@ -1,11 +1,15 @@
+"use client";
 import Feedback from "@/components/feedback/Feedback";
 import Herosection from "@/components/feedback/Herosection";
+import { useRef } from "react";
 
 function Page({}) {
+  const nameSectionRef = useRef(null); // Create ref in parent
   return (
     <>
-      <Herosection />
-      <Feedback />
+      <Herosection nameSectionRef={nameSectionRef} /> {/* Pass the ref */}
+      <Feedback nameSectionRef={nameSectionRef} />
+      {/* pass the ref to feedback component too so it can be attached*/}
     </>
   );
 }
