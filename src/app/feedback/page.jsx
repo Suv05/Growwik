db7d9e496772fe1ae598282722fcc38bc1b73d39
suvam <suvampravu@gmail.com>
2025-02-26 23:34:22 +0530

    feedback page created

---
+++ src/app/feedback/page.jsx
@@ -0,0 +1,13 @@
+import Feedback from "@/components/feedback/Feedback";
+import Herosection from "@/components/feedback/Herosection";
+
+function Page({}) {
+  return (
+    <>
+      <Herosection />
+      <Feedback />
+    </>
+  );
+}
+
+export default Page;
