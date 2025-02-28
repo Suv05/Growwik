e8668a91c1971fe64b6baffd0c78cf53e7e871f8
suvam <suvampravu@gmail.com>
2025-02-28 17:37:14 +0530

    features added

--- src/schema/feedback.model.js
+++ src/schema/feedback.model.js
@@ -1,58 +1,65 @@
 import mongoose from "mongoose";
 const { Schema, model, models } = mongoose;
 
-const feedbackSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide your name"],
-    maxlength: 100,
-    trim: true,
+const feedbackSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide your name"],
+      maxlength: 100,
+      trim: true,
+    },
+    email: {
+      type: String,
+      minlength: 10, // Corrected casing
+      required: [true, "Please provide your email"],
+      lowercase: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Enter a valid email address",
+      ],
+    },
+    role: {
+      type: String,
+    },
+    brandName: {
+      type: String,
+      trim: true,
+    },
+    review: {
+      type: String,
+      required: [true, "Please write your review"],
+      trim: true,
+    },
+    areasForImprovement: {
+      type: String,
+      trim: true,
+    },
+    satisfaction: {
+      type: Number,
+    },
+    communicationRating: {
+      type: String,
+    },
+    metExpectations: {
+      type: String,
+      default: "Yes",
+    },
+    recommendationLikelihood: {
+      type: String,
+      trim: true,
+    },
+    socialMedia: {
+      type: String,
+      trim: true,
+    },
+    isAnonymous: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    minlength: 10, // Corrected casing
-    required: [true, "Please provide your email"],
-    lowercase: true,
-    match: [
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Enter a valid email address",
-    ],
-  },
-  role: {
-    type: String,
-  },
-  brandName: {
-    type: String,
-  },
-  review: {
-    type: String,
-    required: [true, "Please write your review"],
-  },
-  areasForImprovement: {
-    type: String,
-  },
-  satisfaction: {
-    type: String, 
-  },
-  communicationRating: {
-    type: String,
-  },
-  metExpectations: {
-    type: String,
-  },
-  recommendationLikelihood: {
-    type: String,
-    trim: true,
-  },
-  socialMedia: {
-    type: String,
-    trim: true,
-  },
-  isAnonymous: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 const Feedback = models.Feedback || model("Feedback", feedbackSchema);
 export default Feedback;
