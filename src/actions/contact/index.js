b218f941f3273eea0ae2b9fd2e382a94f5bda554
suvam <suvampravu@gmail.com>
2025-09-04 12:13:16 +0530

    email updated in server action for user

--- src/actions/contact/index.js
+++ src/actions/contact/index.js
@@ -1,5 +1,7 @@
 "use server";
 
+import { Resend } from "resend";
+import UserEmail from "@/components/emails/UserEmail";
 import createConnection from "@/db";
 import Contact from "@/schema/contact.model.js";
 
@@ -7,10 +9,15 @@ export async function setContact(data) {
   try {
     // Ensure the MongoDB connection is established
     await createConnection();
+    
 
     const newUser = await Contact.create(data);
 
     if (newUser) {
+
+      // Send email notification to client
+      await sendEmailToClient(newUser);
+
       return {
         status: "success",
         message: "Contact created successfully",
@@ -28,5 +35,28 @@ export async function setContact(data) {
       status: "error",
       message: "Contact creation failed",
     };
+  }
+}
+
+
+// Resend setup
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function sendEmailToClient(contactData) {
+  try {
+    const response = await resend.emails.send({
+      from: "no-reply@growwik.com",
+      to: "growwikmedia@gmail.com", // or process.env.CLIENT_EMAIL
+      subject: "New Contact Request",
+      react: <UserEmail userData={contactData} />,
+    });
+
+    if (response.error) {
+      console.error("❌ Email send error:", response.error);
+    }
+
+    return response;
+  } catch (err) {
+    console.error("❌ Exception while sending email:", err);
   }
 }
